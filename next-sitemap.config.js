4280f716fddf1371210161f4e8aaaca531d1d4e8
ore291 <oreoluwapadonu@gmail.com>
2024-06-01 16:51:35 +0100

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://ylvonis.com', // FIXME: Change to the production URL
+  siteUrl: 'https://ylvonis.mannydigital.net', // FIXME: Change to the production URL
   generateRobotsTxt: true,
 };
